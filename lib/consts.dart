da852cdff9b87f3ed134f84e90af3f64f8d39f9b
Matthias Harzer <matthias.harzer03@gmail.com>
2023-03-10 17:33:28 +0100

    Increased log file limit

--- lib/consts.dart
+++ lib/consts.dart
@@ -4,7 +4,7 @@ const BASE_URL = "https://www.bing.com/HPImageArchive.aspx?format=js";
 
 const WALLPAPER_HISTORY_LIMIT =
     16; // This is a limitation from the bing endpoint
-const LOG_FILE_LINES_LIMIT = 500;
+const LOG_FILE_LINES_LIMIT = 5000;
 
 const BG_WALLPAPER_TASK_ID = "bg_wallpaper_task";
 const BG_TASK_RECURRING_TIME = 1; //h
